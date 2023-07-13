af1f2d9e590611a0db3b76afafba80eac3d06a99
bentike <ajadiyusuf73@gmail.com>
2023-07-13 10:22:16 +0100

    added printList method to Linked Lists

--- linkedList.js
+++ linkedList.js
@@ -1,5 +1,16 @@
 // 10 --> 5 --> 16
 
+
+// BIG O OF SOME BASIC OPERATIONS IN LINKED LIST
+
+// Prepend 0(1)
+// Append  0(1)
+// lookUp  0(n)
+// Insert  0(n)
+// Delete  0(n)
+
+
+
 // let myLinkedList = {
 //     head: {
 //         value: 10,
@@ -34,10 +45,51 @@ class LinkedList {
         return this;
     }
 
+    prepend(value){
+        const newNode = {
+            value,
+            next: this.head
+        }
+        this.head = newNode;
+        this.length++;
+        return this;
+    }
+
+    // Another way to write the prepend Method
+
+    anotherPrepend(value){
+        const newNode = {
+            value,
+            next: null
+        }
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        return this;
+    }
+
+    // Method to print the values of all items in a Linkelist
+    printList(){
+        let myList = [];
+        let currentNode = this.head;
+        while(currentNode !== null){
+            myList.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        return myList;
+    }
+
 }
 
 const firstLinkedList = new LinkedList(10);
 firstLinkedList.append(5);
+firstLinkedList.prepend(15);
+firstLinkedList.prepend(25);
+firstLinkedList.append(95);
+firstLinkedList.anotherPrepend(22)
+firstLinkedList.anotherPrepend(33)
+firstLinkedList.append(47)
+firstLinkedList.prepend(38)
 firstLinkedList.append(16);
 firstLinkedList.append(30);
-console.log(firstLinkedList);
+console.log(firstLinkedList.printList());
