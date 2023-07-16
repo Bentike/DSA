13d334112767ee7642a960e5df461df153f820b6
bentike <ajadiyusuf73@gmail.com>
2023-07-16 15:05:57 +0100

    implemented stack with array

--- stack.js
+++ stack.js
@@ -5,6 +5,7 @@ class Node {
     }
 }
 
+// Stack Implementation with Linked Lists
 class Stack {
     constructor(){
         this.top = null,
@@ -30,11 +31,55 @@ class Stack {
         this.length++;
         return this;
     }
-
+    
+    // pop method removes the last item in the stack
     pop(){
-
+       if(!this.top) return null;
+       if(this.top === this.bottom) this.bottom = null;
+    //    let holdingPointer = this.top;
+       this.top = this.top.next;
+       this.length--;
+       return this;
     }
 }
 
 const myStack = new Stack();
-// console.log(myStack.peek());
+// console.log(myStack.push("google"));
+// console.log(myStack.push("twitter"));
+// console.log(myStack.push("facebook"));
+// console.log(myStack.pop());
+
+// Stack Implementation with Arrays
+class ArrayStack {
+    constructor(){
+       this.stack = [];
+    }
+
+    peek(){
+       if(!this.stack.length) return "empty";
+       return this.stack[this.stack.length - 1];
+    }
+
+    push(val){
+      this.stack.push(val);
+      return this.stack;
+    }
+
+    pop(){
+       this.stack.pop();
+       return this.stack;
+    }
+}
+
+let satckOne = new ArrayStack();
+satckOne.push("Bentike")
+satckOne.push("Tunji")
+satckOne.push("Wale")
+satckOne.push("Dele")
+satckOne.push("Niyi")
+console.log(satckOne);
+console.log(satckOne.peek());
+satckOne.pop()
+satckOne.pop()
+console.log(satckOne);
+console.log(satckOne.peek());
